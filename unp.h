4484dedf268486fc36d061fc6ed484699e1b60cc
kf1127 <kf1127@txstate.edu>
2021-06-13 15:03:02 -0500

    updated unp

--- unp.h
+++ unp.h
@@ -1,1 +1,26 @@
 #define SERV_PORT 9877
+#define MAXLINE 4096
+
+#define SA struct sockaddr  //shortens the typcasts of pointer arguments
+
+
+int     Accept(int, SA *, socklen_t *);
+
+
+int Accept(int fd, struct sockaddr *sa, socklen_t *salenptr)
+{
+    int n;
+
+    again:
+        if ( (n = accept(fd, sa, salenptr)) < 0) {
+#ifdef EPROTO
+            if (errno == EPROTO || errno == ECONNABORTED)
+#else
+            if (errno == ECONNABORTED)
+#endif
+                goto again;
+            else
+                err_sys("accept error");
+        }
+        return(n);
+}
