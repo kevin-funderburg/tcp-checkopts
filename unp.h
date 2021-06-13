3b3a381b8968444398a3a1ff0636beee2073c06f
kevin-funderburg <ksfunderburg@icloud.com>
2021-06-13 16:01:34 -0500

    working on readline

--- unp.h
+++ unp.h
@@ -42,7 +42,9 @@ void    Connect(int, const SA *, socklen_t);
 void    err_sys(const char* msg);
 int     Socket(int family, int type, int protocol);
 void    Writen(int fd, void *ptr, size_t nbytes);
-
+static  ssize_t my_read(int fd, char *ptr);
+ssize_t readline(int fd, void *vptr, size_t maxlen);
+ssize_t readlinebuf(void **vptrptr);
 
 int Accept(int fd, struct sockaddr *sa, socklen_t *salenptr)
 {
@@ -86,6 +88,55 @@ void Writen(int fd, void *ptr, size_t nbytes)
         err_sys("writen error");
 }
 
+
+static ssize_t my_read(int fd, char *ptr)
+{
+    if (read_cnt <= 0) {
+        again:
+            if ( (read_cnt = read(fd, read_buf, sizeof(read_buf))) < 0) {
+                if (errno == EINTR)
+                    goto again;
+                return (-1);
+            } else if (read_cnt == 0)
+                return (0);
+            read_ptr = read_buf;
+    }
+    
+    read_cnt--;
+    *ptr = *read_ptr++;
+    return (1);
+}
+
+ssize_t readline(int fd, void *vptr, size_t maxlen)
+{
+    ssize_t n, rc; 
+    char    c, *ptr;
+
+    ptr=vptr;
+
+    for (n = 1; n < maxlen; n++) {
+        if ( (rc = my_read(fd, &c)) == 1) { 
+            *ptr++ = c;
+            if (c == '\n')
+                break;          //newline is stored, like fgets()
+        } else if (rc == 0) {
+            *ptr = 0;
+            return (n - 1);     //EOF, n - 1 bytes were read
+        } else
+            return (-1);        //error, errno set by read()
+    }
+    
+    *ptr = 0;
+    return(n);
+}
+
+
+ssize_t readlinebuf(void **vptrptr)
+{
+    if(read_cnt) 
+        *vptrptr = read_ptr;
+    return (read_cnt);
+}
 
 /* Fatal error related to system call
  * Print message and terminate */
