bb5e1eab02aa9709ee8a5bd6794c54159dd43d2f
kevin-funderburg <ksfunderburg@icloud.com>
2021-06-13 15:10:09 -0500

    updated unp functions

--- unp.h
+++ unp.h
@@ -1,3 +1,35 @@
+#ifndef UNP_H
+#define UNP_H
+
+#include    <sys/types.h>   /* basic system data types */
+#include    <sys/socket.h>  /* basic socket definitions */
+#if TIME_WITH_SYS_TIME
+#include    <sys/time.h>    /* timeval{} for select() */
+#include    <time.h>        /* timespec{} for pselect() */
+#else
+#if HAVE_SYS_TIME_H
+#include    <sys/time.h>    /* includes <time.h> unsafely */
+#else
+#include    <time.h>        /* old system? */
+#endif
+#endif
+#include    <netinet/in.h>  /* sockaddr_in{} and other Internet defns */
+#include    <arpa/inet.h>   /* inet(3) functions */
+#include    <errno.h>
+#include    <fcntl.h>       /* for nonblocking */
+#include    <netdb.h>
+#include    <signal.h>
+#include    <stdio.h>
+#include    <stdlib.h>
+#include    <string.h>
+#include    <sys/stat.h>    /* for S_xxx file mode constants */
+#include    <sys/uio.h>     /* for iovec{} and readv/writev */
+#include    <unistd.h>
+#include    <sys/wait.h>
+#include    <sys/un.h>      /* for Unix domain sockets */
+#include    <sys/select.h>
+#include    <stdbool.h>
+
 #define SERV_PORT 9877
 #define MAXLINE 4096
 
@@ -5,6 +37,11 @@
 
 
 int     Accept(int, SA *, socklen_t *);
+void	Bind(int, const SA *, socklen_t);
+void    Connect(int, const SA *, socklen_t);
+void    err_sys(const char* msg);
+int     Socket(int family, int type, int protocol);
+void    Writen(int fd, void *ptr, size_t nbytes);
 
 
 int Accept(int fd, struct sockaddr *sa, socklen_t *salenptr)
@@ -24,3 +61,32 @@ int Accept(int fd, struct sockaddr *sa, socklen_t *salenptr)
         }
         return(n);
 }
+
+
+void Bind(int fd, const struct sockaddr *sa, socklen_t salen)
+{
+    if (bind(fd, sa, salen) < 0)
+        err_sys("bind error");
+}
+
+
+int Socket(int family, int type, int protocol)
+{
+    int n;
+
+    if ( (n = socket(family, type, protocol)) < 0)
+        err_sys("socket error");
+    return(n);
+}
+
+
+/* Fatal error related to system call
+ * Print message and terminate */
+
+void err_sys(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+#endif  //UNP_H
