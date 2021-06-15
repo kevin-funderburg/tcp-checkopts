05288655d8add3c0d254edb1cf9eb8a32126ddf9
kevin-funderburg <ksfunderburg@icloud.com>
2021-06-15 14:20:46 -0500

    attempting to run checkopts in the client only

--- unpv13e/tcpcliserv/checkoptscli.c
+++ unpv13e/tcpcliserv/checkoptscli.c
@@ -141,25 +141,71 @@ sock_str_timeval(union val *ptr, int len)
 	return(strres);
 }
 
+// int
+// main(int argc, char **argv)
+// {
+// 	int					sockfd;
+// 	struct sockaddr_in	servaddr;
+// 
+// 	if (argc != 2)
+// 		err_quit("usage: tcpcli <IPaddress>");
+// 
+// 	sockfd = Socket(AF_INET, SOCK_STREAM, 0);
+// 
+// 	bzero(&servaddr, sizeof(servaddr));
+// 	servaddr.sin_family = AF_INET;
+// 	servaddr.sin_port = htons(SERV_PORT);
+// 	Inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+// 
+// 	Connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
+// 
+// 	str_cli(stdin, sockfd);		/* do it all */
+// 
+// 	exit(0);
+// }
+
+
 int
 main(int argc, char **argv)
 {
-	int					sockfd;
-	struct sockaddr_in	servaddr;
+	int					fd;
+	socklen_t			len;
+	struct sock_opts	*ptr;
 
-	if (argc != 2)
-		err_quit("usage: tcpcli <IPaddress>");
+	for (ptr = sock_opts; ptr->opt_str != NULL; ptr++) {
+		printf("%s: ", ptr->opt_str);
+		if (ptr->opt_val_str == NULL)
+			printf("(undefined)\n");
+		else {
+			switch(ptr->opt_level) {
+			case SOL_SOCKET:
+			case IPPROTO_IP:
+			case IPPROTO_TCP:
+				fd = Socket(AF_INET, SOCK_STREAM, 0);
+				break;
+#ifdef	IPV6
+			case IPPROTO_IPV6:
+				fd = Socket(AF_INET6, SOCK_STREAM, 0);
+				break;
+#endif
+#ifdef	IPPROTO_SCTP
+			case IPPROTO_SCTP:
+				fd = Socket(AF_INET, SOCK_SEQPACKET, IPPROTO_SCTP);
+				break;
+#endif
+			default:
+				err_quit("Can't create fd for level %d\n", ptr->opt_level);
+			}
 
-	sockfd = Socket(AF_INET, SOCK_STREAM, 0);
-
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(SERV_PORT);
-	Inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
-
-	Connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
-
-	str_cli(stdin, sockfd);		/* do it all */
-
+			len = sizeof(val);
+			if (getsockopt(fd, ptr->opt_level, ptr->opt_name,
+						   &val, &len) == -1) {
+				err_ret("getsockopt error");
+			} else {
+				printf("default = %s\n", (*ptr->opt_val_str)(&val, len));
+			}
+			close(fd);
+		}
+	}
 	exit(0);
 }
